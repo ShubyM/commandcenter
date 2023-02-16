73968d34d8e00a043a157636178178d14882eafa
Shuby <shubymishra20@gmail.com>
2023-02-16 03:02:25 -0500

    more changes

--- commandcenter/tests/auth/conftest.py
+++ commandcenter/tests/auth/conftest.py
@@ -6,7 +6,9 @@ import functools
 from xprocess import ProcessStarter
 from fastapi.testclient import TestClient
 
-
+@pytest.fixture
+def anyio_backend():
+    return 'asyncio'
 
 @pytest.fixture
 def test_client_factory(anyio_backend_name, anyio_backend_options) -> TestClient:
@@ -17,11 +19,8 @@ def test_client_factory(anyio_backend_name, anyio_backend_options) -> TestClient
         backend=anyio_backend_name,
         backend_options=anyio_backend_options
     )
-    # return TestClient
 
-
-
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session", autouse=True)
 def ldap_server(xprocess):
     class Starter(ProcessStarter):
         timeout = 8
--- commandcenter/tests/auth/test_auth.py
+++ commandcenter/tests/auth/test_auth.py
@@ -9,19 +9,20 @@ from commandcenter.auth.backends.activedirectory import ActiveDirectoryClient
 from commandcenter.auth.backends.activedirectory import ActiveDirectoryBackend
 
 from commandcenter.exceptions import NotConfigured
-from commandcenter.api.dependencies import get_auth_client
-from commandcenter.api.config.auth import (
+from commandcenter.config.auth import (
     CC_AUTH_ALGORITHM,
-    CC_AUTH_SECRET_KEY,
     CC_AUTH_TOKEN_EXPIRE
 )
 
 from fastapi import FastAPI, Request, Depends
 from fastapi.testclient import TestClient
+from fastapi.routing import APIRouter
 
 from starlette.middleware import Middleware
 from starlette.middleware.authentication import AuthenticationMiddleware
 
+
+### Fixtures ###
 # read this in from file
 TestUser = BaseUser(
     username="admin",
@@ -34,127 +35,124 @@ TestUser = BaseUser(
     scopes=set(["ADMIN"]),
 )
 
+
 @pytest.fixture
 def ad_client() -> partial[ActiveDirectoryClient]:
     return partial(
-        ActiveDirectoryClient, 
+        ActiveDirectoryClient,
         domain="",
         hosts=["127.0.0.1:3004"],
         tls=False,
         mechanism="Simple",
     )
 
+
 @pytest.fixture
-def env_token_handler() -> TokenHandler:
+def token_handler() -> TokenHandler:
     return TokenHandler(
         key='secret',
         expire=CC_AUTH_TOKEN_EXPIRE,
         algorithm=CC_AUTH_ALGORITHM
     )
 
+
 @pytest.fixture
 @pytest.mark.asyncio
-async def ad_backend(ad_client, env_token_handler) -> ActiveDirectoryBackend:
+async def ad_backend(ad_client, token_handler) -> ActiveDirectoryBackend:
     return partial(
         ActiveDirectoryBackend,
         client=ad_client(),
-        handler=env_token_handler
+        handler=token_handler
     )
+
 
 @pytest.fixture
 def ad_app(ad_backend) -> FastAPI:
-    return FastAPI(middleware=[Middleware(
-        AuthenticationMiddleware,
-        backend = ad_backend()
-    )])
+    app = FastAPI(
+        title="Test Authentication",
+        middleware=[
+            Middleware(AuthenticationMiddleware, backend=ad_backend())
+        ]
+    )
 
+    router = APIRouter()
+
+    @router.get("/whoami", response_model=BaseUser)
+    def whoami(request: Request):
+        return request.user.dict()
+
+    @router.get("/is_authenticated", response_model=bool)
+    def is_authenticated(request: Request):
+        return request.user.is_authenticated
+
+    @router.get("/requires_admin_scopes", response_model=BaseUser)
+    def requires_admin(request: Request, user: BaseUser = Depends(requires(["ADMIN"]))):
+        return user.dict()
+
+    @router.get("/requires_scopes")
+    def requires_scopes(request: Request, user: BaseUser = Depends(requires(raise_on_no_scopes=True))):
+        return user.dict()
+
+    app.include_router(router)
+
+    return app
+
+
+@pytest.fixture
+def ad_test_client(test_client_factory, ad_app) -> TestClient:
+    with test_client_factory(ad_app) as client:
+        yield client
+
+
+### Tests ###
 @pytest.mark.asyncio
-async def test_authentication_client_get_user(ad_client: partial[ActiveDirectoryClient], ldap_server):
+async def test_authentication_client_get_user(ad_client: partial[ActiveDirectoryClient]):
     admin: BaseUser = await ad_client().get_user("admin")
     assert admin == TestUser
 
 @pytest.mark.asyncio
-async def test_authentication_client_authenticate(ad_client: partial[ActiveDirectoryClient] , ldap_server):
+async def test_authentication_client_authenticate(ad_client: partial[ActiveDirectoryClient]):
     # Assming simple bind with anonymous user, this works
     assert await ad_client().authenticate("", "")
 
 @pytest.mark.asyncio
-async def test_authentication_backend_valid_headers(ad_app, env_token_handler, test_client_factory, ldap_server):
-    @ad_app.get("/", response_model=BaseUser)
-    def endpoint(request: Request):
-        return request.user.dict()
+async def test_authentication_backend_valid_headers(ad_test_client: TestClient, token_handler: TokenHandler):
+    token = token_handler.issue({"sub": "admin"})
+    ad_test_client.headers = {"Authorization": f"Bearer {token}"}
 
-    with test_client_factory(ad_app) as client: 
-        client: TestClient
+    response = ad_test_client.get("/whoami")
 
-        token = env_token_handler.issue({"sub": "admin"})
+    assert response.status_code == 200
 
-        client.headers = {
-            "Authorization": f"Bearer {token}",
-        }
+    user_dict = TestUser.dict()
+    user_dict["scopes"] = list(user_dict["scopes"])
 
-        response = client.get("/")
-
-        assert response.status_code == 200
-        
-        user_dict = TestUser.dict()
-        user_dict["scopes"] = list(user_dict["scopes"])
-
-        assert response.json() == user_dict
-
-@pytest.mark.asyncio
-async def test_authentication_backend_invalid_headers(ad_app, test_client_factory, ldap_server):
-    @ad_app.get("/")
-    def endpoint(request: Request):
-        return {
-            "is_authenticated": request.user.is_authenticated,
-            "username": request.user.display_name
-        }
-
-    with test_client_factory(ad_app) as client: 
-        client: TestClient
-        response = client.get("/")
-
-        assert response.status_code == 200
-
-        user = response.json()
-        assert user["is_authenticated"] == False
+    assert response.json() == user_dict
 
 
 @pytest.mark.asyncio
-async def test_authentication_scopes_valid_scopes(ad_app: FastAPI, test_client_factory: TestClient, env_token_handler: TokenHandler, ldap_server):
-    @ad_app.get("/", dependencies=[Depends(requires(["ADMIN"]))])
-    def endpoint(request: Request):
-        return {"valid": "true"}
+async def test_authentication_backend_invalid_headers(ad_test_client: TestClient):
+    response = ad_test_client.get("/is_authenticated")
+    assert response.status_code == 200
+    assert response.content == b"false"
 
-    with test_client_factory(ad_app) as client:
-        client: TestClient
-        token = env_token_handler.issue({"sub": "admin"})
-        client.headers = {"Authorization": f"Bearer {token}"}
-        assert client.get("/").status_code == 200
 
 @pytest.mark.asyncio
-async def test_authentication_scopes_invalid_scopes(ad_app: FastAPI, test_client_factory: TestClient, env_token_handler: TokenHandler, ldap_server):
-    @ad_app.get("/", dependencies=[Depends(requires(["ADMIN"]))])
-    def endpoint(request: Request):
-        return {"valid": "true"}
+async def test_authentication_scopes_valid_scopes(ad_test_client, token_handler):
+    token = token_handler.issue({"sub": "admin"})
+    ad_test_client.headers = {"Authorization": f"Bearer {token}"}
+    assert ad_test_client.get("requires_admin_scopes").status_code == 200
 
-    with test_client_factory(ad_app) as client:
-        client: TestClient
-        token = env_token_handler.issue({"sub": "nonadmin"})
-        client.headers = {"Authorization": f"Bearer {token}"}
-        assert client.get("/").status_code == 403
 
 @pytest.mark.asyncio
-async def test_authentication_scopes_raise_exception_no_scope(ad_app: FastAPI, test_client_factory: TestClient, env_token_handler: TokenHandler, ldap_server):
-    @ad_app.get("/", dependencies=[Depends(requires(raise_on_no_scopes=True))])
-    def endpoint(request: Request):
-        return {"valid": "true"}
+async def test_authentication_scopes_invalid_scopes(ad_test_client: TestClient, token_handler: TokenHandler):
+    token = token_handler.issue({"sub": "nonadmin"})
+    ad_test_client.headers = {"Authorization": f"Bearer {token}"}
+    assert ad_test_client.get("/requires_admin_scopes").status_code == 403
 
+@pytest.mark.asyncio
+async def test_authentication_scopes_raise_exception_no_scope(ad_test_client: TestClient, token_handler: TokenHandler):
     with pytest.raises(NotConfigured):
-        with test_client_factory(ad_app) as client:
-            client: TestClient
-            token = env_token_handler.issue({"sub": "nonadmin"})
-            client.headers = {"Authorization": f"Bearer {token}"}
-            client.get("/")
-        
+        token = token_handler.issue({"sub": "nonadmin"})
+        ad_test_client.headers = {"Authorization": f"Bearer {token}"}
+        ad_test_client.get("/requires_scopes")
